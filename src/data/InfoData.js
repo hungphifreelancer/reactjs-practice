96dd00c985fdf9b8a5d61bf4f8493bf3d6a8880f
Phi oc <hungphifreelancer@gmail.com>
2021-03-25 16:19:18 +0700

    The practice for UI

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import house1 from '../images/house-1.jpg';
+import house2 from '../images/house-2.jpg';
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae corporis suscipit odit ut illo pariatur illum, qui repellendus quos voluptatibus? Asperiores blanditiis vel ratione cum hic ab eos possimus consequuntur.',
+    paragraphTwo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae corporis suscipit odit ut illo pariatur illum, qui repellendus quos voluptatibus? Asperiores blanditiis vel ratione cum hic ab eos possimus consequuntur.',
+    buttonLabel: 'View Homes',
+    image: house1,
+    reverse: false,
+    delay: 100
+}
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'Repudiandae corporis suscipit odit ut illo pariatur illum, qui repellendus quos voluptatibus? Asperiores blanditiis vel ratione cum hic ab eos possimus consequuntur.',
+    paragraphTwo: 'Repudiandae corporis suscipit odit ut illo pariatur illum, qui repellendus quos voluptatibus? Asperiores blanditiis vel ratione cum hic ab eos possimus consequuntur.',
+    buttonLabel: 'View Designs',
+    image: house2,
+    reverse: true,
+    delay: 300
+}
---
+++ src/data/SliderData.js
@@ -0,0 +1,74 @@
+import house1 from '../images/house-1.jpg';
+import house2 from '../images/house-2.jpg';
+import house3 from '../images/house-3.jpg';
+import house4 from '../images/house-4.jpg';
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa in Bali, Indonesia',
+        price: '$200,000,0',
+        path: '/homes',
+        label: 'View Home',
+        image: house1,
+        alt: 'House'
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$210,801,0',
+        path: '/homes',
+        label: 'View Home',
+        image: house2,
+        alt: 'House'
+    },
+    {
+        title: 'House in Calabasas, California',
+        price: '$8,439,320',
+        path: '/homes',
+        label: 'View Home',
+        image: house3,
+        alt: 'House'
+    },
+    {
+        title: 'House in Hoston, Texas',
+        price: '$6,660,666',
+        path: '/homes',
+        label: 'View Home',
+        image: house4,
+        alt: 'House'
+    },
+];
+
+// export const SliderDataTwo = [
+//     {
+//         title: 'House in Las Vegas, Nevada',
+//         price: '$8,980,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: 'house5',
+//         alt: 'House'
+//     },
+//     {
+//         title: 'House in HCM, VietNam',
+//         price: '$1,200,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: 'house6',
+//         alt: 'House'
+//     },
+//     {
+//         title: 'House in VinhTuong, VinhPhuc',
+//         price: '$5,420,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: 'house7',
+//         alt: 'House'
+//     },
+//     {
+//         title: 'House in HaDong, HaNoi',
+//         price: '$7,440,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: 'house8',
+//         alt: 'House'
+//     },
+// ];
